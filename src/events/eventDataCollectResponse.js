815ccca4c6cf1bd4ae9a2ee712fe840b874b939c
Quenteon <quanteonquantumelysium@gmail.com>
2024-12-01 13:35:26 +0000

    Data processing hub

--- src/events/eventDataCollectResponse.js
+++ src/events/eventDataCollectResponse.js
@@ -1,5 +1,6 @@
 /**
- * src\events\eventDataCollectRequest.js
+ * Data processing hub
+ * src\events\eventDataCollectResponse.js
  */
 
 const {
@@ -7,6 +8,7 @@ const {
   helper,
   errorCodes,
   sendLogRequest,
+  handleDataCollectResponseRequest,
   fileWriter
 } = require('@auto-content-labs/messaging');
 const path = require('path');
@@ -44,41 +46,46 @@ async function saveSourceLog(filePath, logData, append = false) {
 
 /**
  * Handles data collection response events.
- * @param {Object} processedData - The processedData data source.
- * @param {Object} processedData.value - The incoming model data.
- * @param {Object} processedData.value.id - Unique identifier for the data.
- * @param {Object} processedData.value.data - The actual data (must be serialized to string).
- * @param {Object} processedData.value.timestamp - Timestamp of the message.
- * @param {Object} processedData.value.summary - Summary information about the data.
+ * @param {Object} pair - The processedData data source.
+ * @param {Object} pair.value - The incoming model data.
+ * @param {Object} pair.value.id - Unique identifier for the data.
+ * @param {Object} pair.value.data - The actual data (must be serialized to string).
+ * @param {Object} pair.value.timestamp - Timestamp of the message.
+ * @param {Object} pair.value.summary - Summary information about the data.
  */
-async function eventDataCollectResponse({ value, headers } = {}) {
+async function eventDataCollectResponse(pair) {
 
+  const { key, value, headers } = pair
   if (!value) {
     logger.error("No value found in the message");
     return;
   }
 
-  const { id, data, timestamp, summary } = value;
-  const { source, itemCount, dataFormat, processingTime } = summary;
+  const model = await handleDataCollectResponseRequest({ key, value, headers });
+  // Determine the service type and handle parameters accordingly
+  const { id, service, content } = model
+  const { service_id, status_type_id, service_type_id, access_type_id, data_format_id, parameters, measurements } = service;
+  const { content_type, content_length, data } = content;
+  const { protocol, domain, port, path, query_parameters, request_method, rate_limit, rate_limit_window, timeout, retry_count, cache_duration, cache_enabled, max_connections, api_key, logging_enabled, allowed_origins, error_handling, authentication_required, authentication_details, url } = parameters;
+  const { /*service_id,*/ metric_id, measurement_time, measurement_start_time, measurement_end_time, metric_type, metric_value } = measurements
 
-  // Validate message format
-  if (id && data && timestamp && summary) {
+  if (id && service && content) {
     try {
       // Serialize the data if it's an object
-      const serializedData = typeof data === 'object' ? JSON.stringify(data) : data;
+      const serializedData = typeof content.data === 'object' ? JSON.stringify(content.data) : content.data;
 
       // Define the path for the data file
-      const dataFile = `${source}.${dataFormat}`;
+      const dataFile = `${domain}.${content_type}`;
 
       // Save main data to a file
       await saveDataToFile(path.join(__dirname, '../../files', dataFile), serializedData);
 
       // Save source information to a separate log file
       const sourceFile = `sources.csv`;
-      const sourceLog = `${id}, ${source}\n`;
+      const sourceLog = `${id}, ${domain}\n`;
       await saveSourceLog(path.join(__dirname, '../../files/logs', sourceFile), sourceLog, true);
 
-      logger.notice(`[dph] [${id}] ${headers.correlationId.toString()} source: ${source} itemCount: ${itemCount} dataFormat: ${dataFormat} processingTime: ${processingTime}`);
+      logger.notice(`[dph] [${id}] ${headers.correlationId} domain: ${domain} content_length: ${content_length} content_type: ${content_type} ${metric_type}: ${metric_value}`);
 
     } catch (error) {
       logger.error(`Data save error: ${error.message}`, { error });
